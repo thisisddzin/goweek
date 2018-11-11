f0386f47333e03b73fe5043c16b365a887f7b9fa
Adenilson Santos <adenilsonblog@gmail.com>
2018-11-11 01:11:46 -0300

    Aplicação da goweek, ambiente node já feito e react configurado, preparado para ser programado, react native ainda não configurado

---
+++ backend/src/controllers/TweetController.js
@@ -0,0 +1,16 @@
+const Tweet = require('../models/tweet');
+
+module.exports = {
+    async index(req,res) {
+        const tweets = await Tweet.find({}).sort('-createAt');
+        
+        return res.json(tweets);
+    },
+    async store(req,res) {
+        const tweet = await Tweet.create(req.body);
+
+        req.io.emit("tweet", tweet);
+
+        return res.json(tweet);
+    }
+};
