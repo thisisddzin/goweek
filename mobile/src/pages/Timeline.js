f3992c08527c9aca96fbf4d9dcd11b47cae286a8
Adenilson Santos <adenilsonblog@gmail.com>
2018-11-11 13:37:01 -0300

    aplicativo mobile em react-native

---
+++ mobile/src/pages/Timeline.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+// import styles from './styles';
+
+export default class Timeline extends Component {
+  render() {
+    return <Text>Timeline</Text>
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  }
+});
